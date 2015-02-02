19942fae9c2ba91e43de55690184fc71f758af87
zetsubou_0 <zetsubou.zero.0@gmail.com>
2015-02-02 20:22:47 +0300

    add test for sergei

---
+++ src/main/java/com/zetsubou_0/test/siargei/test/p1/GoodbyeWorld.java
@@ -0,0 +1,10 @@
+package com.zetsubou_0.test.siargei.test.p1;
+
+/**
+ * Created by zetsubou_0 on 02.02.15.
+ */
+public class GoodbyeWorld {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
