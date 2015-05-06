a1bc535354c017b067340c593092d9ed3efd652b
zetsubou_0 <zetsubou.zero.0@gmail.com>
2015-05-06 23:24:52 +0300

    add wallpaper

---
+++ java/application/wallpaper/src/main/java/com/zetsubou_0/wallpaper/core/WallpaperChangerWindows.java
@@ -0,0 +1,11 @@
+package com.zetsubou_0.wallpaper.core;
+
+/**
+ * Created by zetsubou_0 on 06.05.15.
+ */
+public class WallpaperChangerWindows implements WallpaperChanger {
+    @Override
+    public void changeWallpaper(String path) {
+
+    }
+}
