17eb98652cc190096886212e100b543c4b2096c3
zetsubou_0 <zetsubou.zero.0@gmail.com>
2015-05-09 16:33:35 +0300

    update

--- java/application/animeList/src/main/java/com/zetsubou_0/animelist/test/AnimeListRunner.java
+++ java/application/animeList/src/main/java/com/zetsubou_0/animelist/test/AnimeListRunner.java
@@ -2,8 +2,18 @@ package com.zetsubou_0.animelist.test;
 
 import com.zetsubou_0.animelist.anime.action.Action;
 import com.zetsubou_0.animelist.anime.action.JcrRead;
+import com.zetsubou_0.animelist.anime.action.ReadAnimeDirectory;
+import com.zetsubou_0.animelist.anime.action.WriteJsonFile;
+import com.zetsubou_0.animelist.anime.constant.ActionConstant;
 import com.zetsubou_0.animelist.anime.exception.ActionException;
 import com.zetsubou_0.animelist.anime.observer.Listener;
+import com.zetsubou_0.animelist.anime.service.metadata.AnimeAnilist;
+import com.zetsubou_0.animelist.anime.service.metadata.AnimeData;
+import com.zetsubou_0.animelist.anime.service.metadata.AnimeFileSystem;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zetsubou_0 on 01.05.15.
@@ -16,34 +26,34 @@ public class AnimeListRunner implements Listener {
     }
 
     public void process() {
-//        try {
-//            AnimeData fileSystem = new AnimeFileSystem();
-//            Action read = new ReadAnimeDirectory();
-//            fileSystem.pullData(read);
-//
-//            AnimeData aniList = new AnimeAnilist();
-//            Action write = new WriteJsonFile(read);
-//            Map<String, Object> params = new HashMap<>();
-//            params.put(ActionConstant.Observer.LISTENER, this);
-//            write.addParams(params);
-//            aniList.pullData(write);
-//
-//            // wait all threads
-//            synchronized(AnimeListRunner.class) {
-//                AnimeListRunner.class.wait(TimeUnit.MINUTES.toMillis(5));
-//            }
-//        } catch (ActionException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-        Action jcrRead = new JcrRead();
         try {
-            jcrRead.perform();
+            AnimeData fileSystem = new AnimeFileSystem();
+            Action read = new ReadAnimeDirectory();
+            fileSystem.pullData(read);
+
+            AnimeData aniList = new AnimeAnilist();
+            Action write = new WriteJsonFile(read);
+            Map<String, Object> params = new HashMap<>();
+            params.put(ActionConstant.Observer.LISTENER, this);
+            write.addParams(params);
+            aniList.pullData(write);
+
+            // wait all threads
+            synchronized(AnimeListRunner.class) {
+                AnimeListRunner.class.wait(TimeUnit.MINUTES.toMillis(5));
+            }
         } catch (ActionException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+//        Action jcrRead = new JcrRead();
+//        try {
+//            jcrRead.perform();
+//        } catch (ActionException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Override
