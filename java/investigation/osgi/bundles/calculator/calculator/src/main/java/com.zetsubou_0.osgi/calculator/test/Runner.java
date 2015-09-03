fb22f961837eb0cc6eabf854b8fc711dacd34034
Kiryl_Lutsyk <zetsubou.zero.0@gmail.com>
2015-09-03 18:04:51 +0300

    osgi test was updated

--- java/investigation/osgi/bundles/calculator/calculator/src/main/java/com.zetsubou_0.osgi/calculator/test/Runner.java
+++ java/investigation/osgi/bundles/calculator/calculator/src/main/java/com.zetsubou_0.osgi/calculator/test/Runner.java
@@ -5,6 +5,7 @@ import com.zetsubou_0.osgi.api.exception.OperationException;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,30 +16,37 @@ public class Runner {
 
     public static void main(String[] args) {
         String[] operations = {"+", "-"};
-        String input = "10 + 3 + 10.2 - 1 + 10";
+        String input = "10 + 3 - 7 - 4 + 100";
         List<String> op = Arrays.asList(operations);
         try {
-            System.out.println(execute(input, 0));
+            System.out.println(execute(input, 0, null));
         } catch (OperationException e) {
             e.printStackTrace();
         }
     }
 
-    private static double execute(String str, double initValue) throws OperationException {
-        String[] operations = str.split(REG_EXP);
+    private static double execute(String str, double initValue, String lastOperation) throws OperationException {
+        String[] operationsArray = str.split(REG_EXP);
+        List<String> operations = Arrays.asList(operationsArray);
+        Collections.reverse(operations);
         for(String operation : operations) {
             if(StringUtils.isBlank(operation)) {
                 continue;
             }
-            String val = str.substring(0, str.indexOf(operation));
-            return executeCommand(operation, initValue, execute(str.replace(val + operation, StringUtils.EMPTY), Double.parseDouble(val.trim())));
+            String val = str.substring(str.lastIndexOf(operation) + 1, str.length());
+            StringBuilder commandWithDigit = new StringBuilder();
+            commandWithDigit.append("[");
+            commandWithDigit.append(operation);
+            commandWithDigit.append("]");
+            commandWithDigit.append(val.replace(".", "[.]"));
+            commandWithDigit.append("$");
+            return executeCommand(lastOperation, initValue, execute(str.replaceAll(commandWithDigit.toString(), StringUtils.EMPTY), Double.parseDouble(val.trim()), operation));
         }
-//        if(operations.length == 0) {
-//            try{
-//                executeCommand(operation, initValue, execute(str.replace(val + operation, StringUtils.EMPTY), Double.parseDouble(val.trim())));
-//                initValue += Double.parseDouble(str.trim());
-//            } catch (Exception e) {}
-//        }
+        if(operations.size() == 0) {
+            try{
+                return executeCommand(lastOperation, initValue, Double.parseDouble(str.trim()));
+            } catch (Exception e) {}
+        }
         return initValue;
     }
 
@@ -48,6 +56,9 @@ public class Runner {
             operation = new Add();
         } else if("-".equals(command)) {
             operation = new Minus();
+        }
+        if(operation == null) {
+            return right;
         }
         return operation.execute(left, right);
     }
