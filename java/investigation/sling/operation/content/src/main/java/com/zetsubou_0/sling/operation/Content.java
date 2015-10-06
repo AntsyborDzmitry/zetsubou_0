fd6d95f1fbbcdff8171fcbe03fe54c1956909968
Kiryl_Lutsyk <zetsubou.zero.0@gmail.com>
2015-10-06 16:51:44 +0300

    update sling

--- java/investigation/sling/operation/content/src/main/java/com/zetsubou_0/sling/operation/Content.java
+++ java/investigation/sling/operation/content/src/main/java/com/zetsubou_0/sling/operation/Content.java
@@ -1,14 +1,16 @@
 package com.zetsubou_0.sling.operation;
 
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.*;
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.servlets.post.*;
 
 import javax.jcr.*;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -16,10 +18,11 @@ import java.util.List;
  */
 @Component(immediate = true)
 @Service(value = PostOperation.class)
-@Property(name=PostOperation.PROP_OPERATION_NAME, value="slingTestContent")
+@org.apache.felix.scr.annotations.Property(name=PostOperation.PROP_OPERATION_NAME, value="slingTestContent")
 public class Content extends AbstractPostOperation {
     private static final String PREFIX = "page-%s";
     private static final String CONTENT_ROOT = "/content";
+    private static final String BUNDLE = "/content/system/target/sling-operation-content-1.0-SNAPSHOT.jar";
 
     @Override
     protected void doRun(SlingHttpServletRequest slingHttpServletRequest, PostResponse postResponse, List<Modification> list) throws RepositoryException {
@@ -42,7 +45,24 @@ public class Content extends AbstractPostOperation {
                     }
                 }
             }
-            session.save();
+
+            Resource jar = slingHttpServletRequest.getResourceResolver().getResource(BUNDLE);
+            File file = jar.adaptTo(File.class);
+            if(file != null) {
+                try {
+                    ValueFactory valueFactory = session.getValueFactory();
+                    Node fileContainer = jcrContent.addNode(file.getName(), JcrConstants.NT_FILE);
+                    Node fileContent = fileContainer.addNode(JcrConstants.JCR_CONTENT, JcrConstants.NT_RESOURCE);
+                    Calendar lastModified = Calendar.getInstance ();
+                    lastModified.setTimeInMillis (file.lastModified ());
+                    BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+                    fileContent.setProperty(JcrConstants.JCR_DATA, valueFactory.createBinary(in));
+                    fileContent.setProperty(JcrConstants.JCR_LASTMODIFIED, lastModified);
+                    session.save();
+                } catch (java.io.IOException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
         }
     }
 }
