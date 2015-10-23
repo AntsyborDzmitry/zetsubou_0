b027d62204676df8d6f7167bc2c00444ef041915
Kiryl_Lutsyk <zetsubou.zero.0@gmail.com>
2015-10-23 17:53:49 +0300

    update scala test

--- scala/investigation/scala_for_impatient/Test.scala
+++ scala/investigation/scala_for_impatient/Test.scala
@@ -1,6 +1,2 @@
-var sum: Int = 0;
-for (ch <- "Hello world") {
-  println("ch = " + ch);
-  sum += ch;
-}
-println(sum);
+var a: Array[Int] = Array(-24, -9, -20, 13, 24, 7, -6, -13, -21, 1, 6, 2, -6, -13, -19, -2, 11, 20, 2, -17, 29, 14, 19, 8, 3, 0, -2, -21, 23, 14);
+a.filter(_ > 0).toBuffer ++= a.filter(_ <= 0)
