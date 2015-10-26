a0c184b43f82e0cde318df8f66e854e296ecce6e
Kiryl_Lutsyk <zetsubou.zero.0@gmail.com>
2015-10-26 17:57:51 +0300

    update scala test

--- scala/investigation/scala_for_impatient/Test.scala
+++ scala/investigation/scala_for_impatient/Test.scala
@@ -1,4 +1,4 @@
-import com.zetsubou_0.scala.test.{Network, Counter, Person}
+import com.zetsubou_0.scala.test.{BankAccount, Network, Counter, Person, Time}
 
 var a: Array[Int] = Array(-24, -9, -20, 13, 24, 7, -6, -13, -21, 1, 6, 2, -6, -13, -19, -2, 11, 20, 2, -17, 29, 14, 19, 8, 3, 0, -2, -21, 23, 14);
 a.filter(_ > 0).toBuffer ++= a.filter(_ <= 0)
@@ -21,7 +21,9 @@ val prices = Map("a" -> 100.0, "b" -> 250.0, "c" -> 80.50)
 // scala.collection.immutable.Map[String,Double] = Map(a -> 90.0, b -> 225.0, c -> 72.45)
 val pricesDiscount = for ((k, v) <- prices) yield (k, v * 0.9)
 
-
+def sopl(str: String): Unit = {
+  Console.println(str)
+}
 
 //val in = new java.util.Scanner(new java.io.File("d:\\temp\\scala\\incomplete"))
 //val m: scala.collection.mutable.Map[String, Int] = scala.collection.mutable.Map()
@@ -47,3 +49,14 @@ new Person().action(p)
 
 val network = new Network("Kiryl")
 Console.println(network.member.description)
+
+val account = new BankAccount("Test")
+Console.println(account.balance)
+account.deposit(100)
+Console.println(account.balance)
+Console.println(account.withdraw(50))
+Console.println(account.withdraw(150))
+
+val t1 = new Time(5, 54)
+val t2 = new Time(6, 10)
+sopl(t1.before(t2).toString)
---
+++ scala/investigation/scala_for_impatient/src/com/zetsubou_0/scala/test/Time.scala
@@ -0,0 +1,16 @@
+package com.zetsubou_0.scala.test
+
+/**
+ * Created by Kiryl_Lutsyk on 10/26/2015.
+ */
+class Time(val h: Int, val m: Int) {
+  if((h < 0 || h > 23) && (m < 0 || m > 59)) throw new IllegalArgumentException()
+
+  def before(other: Time): Boolean = {
+    other.minutes - minutes > 0
+  }
+
+  private def minutes = {
+    h * 100 + m
+  }
+}
