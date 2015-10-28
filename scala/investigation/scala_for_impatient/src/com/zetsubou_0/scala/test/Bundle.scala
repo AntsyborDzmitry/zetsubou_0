5a07212081c087256b98ab65261fb9fcebd253d3
Kiryl_Lutsyk <zetsubou.zero.0@gmail.com>
2015-10-28 11:17:34 +0300

    update scala test

--- scala/investigation/scala_for_impatient/src/com/zetsubou_0/scala/test/Bundle.scala
+++ scala/investigation/scala_for_impatient/src/com/zetsubou_0/scala/test/Bundle.scala
@@ -6,13 +6,25 @@ import collection.mutable.ArrayBuffer
  * Created by Kiryl_Lutsyk on 10/27/2015.
  */
 class Bundle extends Item {
-    val items: ArrayBuffer[Item] = ArrayBuffer[Item]
+    val items: ArrayBuffer[Item] = new ArrayBuffer()
 
-    override def price: Int = {0}
+    override def price: Int = {
+        items.foldLeft(0) { (x, y) =>
+            x + y.price
+        }
+    }
 
-    override def description: String = {""}
+    override def description: String = {
+        items.foldLeft("") { (x, y) =>
+            x + y.description + " "
+        }
+    }
 
     def add(item: Item): Unit = {
         items += item
+    }
+
+    def remove(item: Item): Unit = {
+        items -= item
     }
 }
--- scala/investigation/scala_for_impatient/src/com/zetsubou_0/scala/test/Test.scala
+++ scala/investigation/scala_for_impatient/src/com/zetsubou_0/scala/test/Test.scala
@@ -5,8 +5,17 @@ package com.zetsubou_0.scala.test
  */
 object Test {
     def main (args: Array[String]): Unit = {
-        val item1: Item = new SimpleItem(400, "apple")
+        val item1 = new SimpleItem(400, "apple")
         Console.println(item1.price)
         Console.println(item1.description)
+        val item2 = new SimpleItem(350, "banana")
+        Console.println(item2.price)
+        Console.println(item2.description)
+        val item3 = new Bundle()
+        item3.add(item1)
+        item3.add(item2)
+        item3.add(item2)
+        Console.println(item3.price)
+        Console.println(item3.description)
     }
 }
