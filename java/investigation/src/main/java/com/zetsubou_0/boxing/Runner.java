10f957bd472ce309ae046cb1e8b3ffc59e4d344d
Kiryl Lutsyk <Kiryl_Lutsyk@epam.com>
2015-11-11 16:29:07 +0300

    update investigation

---
+++ java/investigation/src/main/java/com/zetsubou_0/boxing/Runner.java
@@ -0,0 +1,10 @@
+package com.zetsubou_0.boxing;
+
+/**
+ * Created by Kiryl_Lutsyk on 11/11/2015.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        int x = (Integer) null;
+    }
+}
