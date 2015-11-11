f8f6f192a3debb75ad76cb36e725b4b3f07f9c93
Kiryl Lutsyk <Kiryl_Lutsyk@epam.com>
2015-11-11 16:25:30 +0300

    update aem

---
+++ java/aem/aem.made/bundle/src/main/java/com/zetsubou_0/provider/CustomBundleProvider.java
@@ -0,0 +1,7 @@
+package com.zetsubou_0.provider;
+
+/**
+ * Created by Kiryl_Lutsyk on 11/6/2015.
+ */
+public class CustomBundleProvider {
+}
--- java/aem/aem.made/bundle/src/main/java/com/zetsubou_0/servlet/TestServler.java
+++ java/aem/aem.made/bundle/src/main/java/com/zetsubou_0/servlet/TestServler.java
@@ -1,29 +1,43 @@
 package com.zetsubou_0.servlet;
 
-import com.zetsubou_0.model.PersonModel;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.i18n.ResourceBundleProvider;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Created by Kiryl_Lutsyk on 10/30/2015.
  */
 @SlingServlet(
         resourceTypes = "sling/servlet/default",
-        paths = "/content/qwe"
+        selectors = "aem",
+        extensions = "test"
 )
 public class TestServler extends SlingAllMethodsServlet {
+    @Reference(cardinality = ReferenceCardinality.OPTIONAL_MULTIPLE, bind = "bind", unbind = "unbind")
+    ResourceBundleProvider resourceBundleProvider;
+
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
-        Resource resource = request.getRequestPathInfo().getSuffixResource();
-        if (resource != null) {
-            PersonModel person = resource.adaptTo(PersonModel.class);
-            response.getWriter().print(person);
+        ResourceBundle resourceBundle = resourceBundleProvider.getResourceBundle("dhl", new Locale("en"));
+        response.getWriter().write(resourceBundle.getString("common.edit"));
+    }
+
+    protected void bind(ResourceBundleProvider resourceBundleProvider) {
+        if (resourceBundleProvider.getClass().getName().endsWith("JcrResourceBundleProvider")) {
+            this.resourceBundleProvider = resourceBundleProvider;
         }
+    }
+
+    protected void unbind(ResourceBundleProvider resourceBundleProvider) {
+        this.resourceBundleProvider = null;
     }
 }
