054a29fb7acf52778078e11e1cd386a3c531c11b
Kiryl Lutsyk <Kiryl_Lutsyk@epam.com>
2016-09-14 14:06:42 +0300

    Auto - test
    1. Driver update

--- java/investigation/auto-tests/src/main/java/com/vi/ni/autotest/appium/Runner.java
+++ java/investigation/auto-tests/src/main/java/com/vi/ni/autotest/appium/Runner.java
@@ -1,8 +1,10 @@
 package com.vi.ni.autotest.appium;
 
+import org.openqa.selenium.WebDriver;
+
 public class Runner {
 
     public static void main(String[] args) {
-        Driver.CHROME.getWebDriver();
+        WebDriver webDriver = Driver.CHROME.getWebDriver();
     }
 }
