3ea937ea37efba1a400bf465b1e6eeed9ad02813
zetsubou_0 <zetsubou.zero.0@gmail.com>
2016-09-26 00:48:49 +0300

    golf

--- java/application/carChooser/src/main/java/com/zetsubou_0/carchooser/core/api/RatingService.java
+++
@@ -1,7 +0,0 @@
-package com.zetsubou_0.carchooser.core.api;
-
-import com.zetsubou_0.carchooser.core.been.car.Car;
-
-public interface RatingService {
-    int getTotalRating(Car car, boolean withDisadvantages);
-}
