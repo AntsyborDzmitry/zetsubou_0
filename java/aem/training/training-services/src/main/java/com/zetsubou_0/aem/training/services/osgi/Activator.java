f1cfca45be2c632ebe5f3f6d7f974859c0181001
Kiryl Lutsyk <kiryl.lutsyk@axamit.com>
2016-11-02 18:48:40 +0300

    init aem training

---
+++ java/aem/training/training-services/src/main/java/com/zetsubou_0/aem/training/services/osgi/Activator.java
@@ -0,0 +1,19 @@
+package com.zetsubou_0.aem.training.services.osgi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.squeakysand.osgi.framework.BasicBundleActivator;
+
+/**
+ * Bundle activator for com.zetsubou_0.aem.training - training-services.
+ */
+public class Activator extends BasicBundleActivator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+    public Activator() {
+		super(LOG);
+	}
+
+}
