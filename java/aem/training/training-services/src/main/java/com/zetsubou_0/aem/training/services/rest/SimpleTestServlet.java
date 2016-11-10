d85c1e61c819d40723af3b75a098ef925fae8b05
Kiryl Lutsyk <kiryl.lutsyk@axamit.com>
2016-11-10 18:18:49 +0300

    update aem training

--- java/aem/training/training-services/src/main/java/com/zetsubou_0/aem/training/services/rest/SimpleTestServlet.java
+++ java/aem/training/training-services/src/main/java/com/zetsubou_0/aem/training/services/rest/SimpleTestServlet.java
@@ -1,12 +1,15 @@
 package com.zetsubou_0.aem.training.services.rest;
 
+import com.zetsubou_0.aem.training.sling.model.TestModel;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @SlingServlet(paths = "/bin/aem/training/test")
 public class SimpleTestServlet extends SlingSafeMethodsServlet {
@@ -14,6 +17,17 @@ public class SimpleTestServlet extends SlingSafeMethodsServlet {
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().println("I'm working");
+        Resource resource = request.getRequestPathInfo().getSuffixResource();
+        PrintWriter writer = response.getWriter();
+        if (resource == null) {
+            writer.println("Resource is null");
+            return;
+        }
+        TestModel testModel = resource.adaptTo(TestModel.class);
+        if (testModel == null) {
+            writer.println("Model is null");
+            return;
+        }
+        writer.println("I'm working." + "Type: " + testModel.getType());
     }
 }
--- java/aem/training/training-services/src/main/java/com/zetsubou_0/aem/training/sling/model/TestModel.java
+++ java/aem/training/training-services/src/main/java/com/zetsubou_0/aem/training/sling/model/TestModel.java
@@ -1,22 +1,29 @@
 package com.zetsubou_0.aem.training.sling.model;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
-@Model(adaptables = Resource.class)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class TestModel {
 
-    private Resource resource;
-
     @Inject
-    @Optional
     @Named("sling:resourceType")
     private String type;
 
+    @Inject
+    private String x;
+
+//    @Self
+    private Resource resource;
+
+    public TestModel(Resource resource) {
+        this.resource = resource;
+    }
 
     public String getType() {
         return type;
